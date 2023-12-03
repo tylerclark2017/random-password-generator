fac26f7a2f1472dce72eb363aaab4186abd42ce8
Tyler Clark <tylerc111@gmail.com>
2023-12-03 17:26:21 -0500

    added initial JavaScript functionality, to be updated.

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
 // Assignment code here
+var passwordLength = parseInt(prompt("Enter the length of the password:"));
+var includeUppercase = confirm("Include uppercase letters?");
+var includeLowercase = confirm("Include lowercase letters?");
+var includeNumbers = confirm("Include numbers?");
+var includeSpecialCharacters = confirm("Include special characters?");
+var uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+var numbers = "0123456789";
+var specialCharacters = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
+var password = "";
 
+var characterSet = ";"
+if (includeUppercase) {
+  characterSet += uppercaseLetters;
+  if (includeLowercase) {
+    characterSet += lowercaseLetters;
+  }
+  if (includeNumbers) {
+    characterSet += numbers;
+  }
+  if (includeSpecialCharacters) {
+    characterSet +=specialCharacters;
+  }
+}
+
+for (var i = 0; i < passwordLength; i++) {
+  var randomIndex = Math.floor (Math.random() * characterSet.passwordLength);
+  password += characterSet[randomIndex];
+  {
+    return password;
+  }
+}
+
+
+console.log(password);
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
